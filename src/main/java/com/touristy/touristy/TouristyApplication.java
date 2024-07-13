3786675d554d50fe08e53e2572f4bb9b913530e5
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-07-13 18:33:11 +0100

    first commit

---
+++ src/main/java/com/touristy/touristy/TouristyApplication.java
@@ -0,0 +1,13 @@
+package com.touristy.touristy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/touristy/touristy/TouristyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touristy.touristy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
