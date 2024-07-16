f4e5d6ebf9731b7b33b4787e9c44cb513f46cba1
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-07-16 15:51:45 +0100

    reformat code

--- src/main/java/com/touristy/touristy/converters/ListImagesConverter.java
+++ src/main/java/com/touristy/touristy/converters/ListImagesConverter.java
@@ -7,17 +7,17 @@ import java.util.List;
 public class ListImagesConverter implements AttributeConverter<List<String>, String> {
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
-       // List<String> to String
-       String images = "";
-       for (String image : attribute) {
-          images += image + ",";
-       }
-       return images;
+        // List<String> to String
+        String images = "";
+        for (String image : attribute) {
+            images += image + ",";
+        }
+        return images;
     }
 
-   @Override
-   public List<String> convertToEntityAttribute(String s) {
-       List<String> images = List.of(s.split(","));
-      return images;
-   }
+    @Override
+    public List<String> convertToEntityAttribute(String s) {
+        List<String> images = List.of(s.split(","));
+        return images;
+    }
 }
