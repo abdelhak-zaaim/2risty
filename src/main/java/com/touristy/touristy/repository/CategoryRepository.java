8a6c88988bfd2e7d79e91e9936ec19338567e706
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-07-23 20:46:48 +0100

    update

---
+++ src/main/java/com/touristy/touristy/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.touristy.touristy.repository;
+
+import com.touristy.touristy.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+}
