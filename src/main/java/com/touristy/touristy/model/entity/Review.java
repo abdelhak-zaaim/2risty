f8393a8500468e1610443de02f55a3f063731a9a
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-07-24 19:45:43 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/Review.java
+++ src/main/java/com/touristy/touristy/model/entity/Review.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serial;
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
