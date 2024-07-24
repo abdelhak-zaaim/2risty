cee1eca9ea5333b7c173aba32fc9003b536c68fb
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-07-24 19:46:00 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/Category.java
+++ src/main/java/com/touristy/touristy/model/entity/Category.java
@@ -3,7 +3,6 @@ package com.touristy.touristy.model.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
 import java.io.Serial;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
