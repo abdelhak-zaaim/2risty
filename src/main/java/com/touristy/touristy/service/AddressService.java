835f1f1732fc3fd2d416e73789e1b87d0337bb3f
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-02 16:38:51 +0100

    update AddressService

--- src/main/java/com/touristy/touristy/service/AddressService.java
+++ src/main/java/com/touristy/touristy/service/AddressService.java
@@ -1,9 +1,15 @@
 package com.touristy.touristy.service;
 
 
+import com.touristy.touristy.repository.AddressRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
 
 }
