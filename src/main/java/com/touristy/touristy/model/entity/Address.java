c2c4c98e0b1463bb5b9851fcdcc07e75b2208737
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-12 18:35:50 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/Address.java
+++ src/main/java/com/touristy/touristy/model/entity/Address.java
@@ -2,12 +2,18 @@ package com.touristy.touristy.model.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
 import java.io.Serial;
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Address implements java.io.Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -37,5 +43,21 @@ public class Address implements java.io.Serializable {
     @PreUpdate
     public void preUpdate() {
         this.updatedAt = Instant.now();
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer().getPersistentClass() : object.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Address address = (Address) object;
+        return getId() != null && Objects.equals(getId(), address.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
