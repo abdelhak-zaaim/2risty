b675cc6eba054637b64f184ee345ddffcb2739c1
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-14 15:08:47 +0100

    update

--- src/main/java/com/touristy/touristy/model/request/AddressDto.java
+++ src/main/java/com/touristy/touristy/model/request/AddressDto.java
@@ -1,13 +1,8 @@
 package com.touristy.touristy.model.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Value;
 
 import java.io.Serializable;
-
-/**
- * DTO for {@link com.touristy.touristy.model.entity.Address}
- */
 
 public record AddressDto(String id, String street, @NotNull String city, @NotNull String state,
                          @NotNull String country) implements Serializable {
