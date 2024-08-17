176afd4bbc3c58c2424c5e9a63c12cecf787071d
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-17 15:06:08 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/Category.java
+++ src/main/java/com/touristy/touristy/model/entity/Category.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
-
 import java.io.Serial;
 import java.time.Instant;
 import java.util.Objects;
