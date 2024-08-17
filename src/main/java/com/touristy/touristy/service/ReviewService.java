dd29d7598985df23797790d7e4e8a3320f52a1ab
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-17 15:07:32 +0100

    update

--- src/main/java/com/touristy/touristy/service/ReviewService.java
+++ src/main/java/com/touristy/touristy/service/ReviewService.java
@@ -8,17 +8,17 @@ import java.util.Optional;
 
 @Service
 public class ReviewService {
-   private final ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
     public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
-    public void deleteReviewById(Long id){
-       reviewRepository.deleteById(id);
+    public void deleteReviewById(Long id) {
+        reviewRepository.deleteById(id);
     }
 
-    public Optional<Review> findReviewById(Long id){
-       return reviewRepository.findById(id);
+    public Optional<Review> findReviewById(Long id) {
+        return reviewRepository.findById(id);
     }
 }
