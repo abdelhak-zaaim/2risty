39b38c73b6c81b894427d791380e3572728125f8
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-17 15:07:35 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/TouristicPlace.java
+++ src/main/java/com/touristy/touristy/model/entity/TouristicPlace.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.io.Serial;
-
 import java.time.Instant;
 import java.util.List;
 
