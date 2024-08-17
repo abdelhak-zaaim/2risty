0cc6fb7d6454e4d0d931afd18c21a2892c79396e
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-17 15:06:37 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/Address.java
+++ src/main/java/com/touristy/touristy/model/entity/Address.java
@@ -11,9 +11,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 @Entity
-@Table(name = "Address", uniqueConstraints = {
-        @UniqueConstraint(name = "uc_address_id", columnNames = {"id"}),
-})
+
 @Getter
 @Setter
 @ToString
