fdaf5791e2915da431a41f668cdb80c14ea1f41b
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-17 15:07:38 +0100

    update

--- src/main/java/com/touristy/touristy/repository/TouristyPlaceRepository.java
+++ src/main/java/com/touristy/touristy/repository/TouristyPlaceRepository.java
@@ -1,6 +1,5 @@
 package com.touristy.touristy.repository;
 
-import com.touristy.touristy.model.entity.Contact;
 import com.touristy.touristy.model.entity.TouristicPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
