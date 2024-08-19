1ce25948044b0a1ae2c8a6d5e308190c04571500
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-08-19 09:07:51 +0100

    update

--- src/main/java/com/touristy/touristy/model/entity/Review.java
+++ src/main/java/com/touristy/touristy/model/entity/Review.java
@@ -2,21 +2,23 @@ package com.touristy.touristy.model.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Locked;
 
 import java.io.Serial;
 import java.time.Instant;
 
 @Entity
-@Table(name = "Review", indexes = {
-        @Index(name = "idx_review_id", columnList = "id")
-})
+
 public class Review implements java.io.Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String comment;
+
     @Column(nullable = false)
     @NotNull
     private int stars;
@@ -24,7 +26,9 @@ public class Review implements java.io.Serializable {
     private Instant date;
 
     @PrePersist
+    @Locked.Read
     public void prePersist() {
         this.date = Instant.now();
     }
+
 }
