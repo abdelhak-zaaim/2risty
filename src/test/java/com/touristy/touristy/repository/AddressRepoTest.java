f01102179778fea60df7ce888403d245eb6b379b
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-09-27 13:03:45 +0100

    update

--- src/test/java/com/touristy/touristy/repository/AddressRepoTest.java
+++ src/test/java/com/touristy/touristy/repository/AddressRepoTest.java
@@ -4,8 +4,7 @@ import com.touristy.touristy.model.entity.Address;
 import com.touristy.touristy.service.AddressService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
