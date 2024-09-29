53309e5a90470ce9f2e7062b5decc01316bcdbbb
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-09-29 16:54:13 +0100

    update

--- src/main/java/com/touristy/touristy/TouristyApplication.java
+++ src/main/java/com/touristy/touristy/TouristyApplication.java
@@ -3,6 +3,8 @@ package com.touristy.touristy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Type;
+
 @SpringBootApplication
 public class TouristyApplication {
 
