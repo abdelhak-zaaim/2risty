2f91a52a4a2edbf721a9194ef6680614321b1c9d
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-10-17 19:07:09 +0100

    Update Address.java

--- src/main/java/com/touristy/touristy/model/entity/Address.java
+++ src/main/java/com/touristy/touristy/model/entity/Address.java
@@ -20,7 +20,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class Address implements java.io.Serializable {
     @Serial
-    private static final long serialVersionUID = 77680071L;
+    private static final long serialVersionUID = 776780071L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
