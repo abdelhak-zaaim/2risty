71cb6ebbabf1a177d1d803440fbbd7a2c6a7980a
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-10-23 19:51:57 +0100

    Update Review.java

--- src/main/java/com/touristy/touristy/model/entity/Review.java
+++ src/main/java/com/touristy/touristy/model/entity/Review.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 @Entity
 public class Review implements java.io.Serializable {
     @Serial
-    private static final long serialVersionUID = 125_236_42;
+    private static final long serialVersionUID = 125_2766_42;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
