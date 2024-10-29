6a5a6ffb0f5259d0ba58a0885dea5b48351b5cf2
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-10-29 20:18:29 +0100

    Update Contact.java

--- src/main/java/com/touristy/touristy/model/entity/Contact.java
+++ src/main/java/com/touristy/touristy/model/entity/Contact.java
@@ -18,7 +18,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class Contact implements java.io.Serializable {
     @Serial
-    private static final long serialVersionUID = 179868L;
+    private static final long serialVersionUID = 17988868L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
