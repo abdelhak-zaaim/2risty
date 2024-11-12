ed57e8832ea8f3d938814495dbe7de575b22ccba
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-11-12 16:31:04 +0100

    update

--- src/main/java/com/touristy/touristy/repository/AddressRepository.java
+++ src/main/java/com/touristy/touristy/repository/AddressRepository.java
@@ -3,9 +3,13 @@ package com.touristy.touristy.repository;
 import com.touristy.touristy.model.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AddressRepository extends JpaRepository<Address, String> {
     Address findByCity(String city);
     Address findByStreet(String street);
     Address findByState(String state);
     Address findByCountry(String country);
+    Optional<Address> findById(String id);
+
 }
